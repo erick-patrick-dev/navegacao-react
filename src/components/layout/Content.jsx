83618c9131983ecd8aa19d51b432b30ff1eeefe0
Homura-san <erickbispo@ufba.br>
2025-10-07 19:50:52 -0300

    Navegação simples

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import './Content.css'
+import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
+
+import Home from "../../views/examples/Home";
+// import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+// import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
-    <main className="Content">
-        <h1>
-            Componente Content
-        </h1>
-    </main>
-)
+  <main className="Content">
+    {/* Toda essa parte está diferente da do professor, analisem as diferenças */}
+    <Routes>
+      <Route path="/about" element={<About />} />
+      {/* <Route path="/param/:id" element={<Param />} /> */}
+      <Route path="/" exact element={<Home />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+    {/* --------------------------------------------------- */}
+  </main>
+);
 
 export default Content;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import './App.css'
+
 import Menu from "../components/layout/Menu.jsx";
 import Content from "../components/layout/Content.jsx";
 
 const App = props => (
     <div className="App">
-        <Menu />
-        <Content />
+        <BrowserRouter>
+            <Menu />
+            <Content />
+        </BrowserRouter>
     </div>
 )
 
