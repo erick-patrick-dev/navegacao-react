d8d28bf77fc07c099e2e570852e43eee6fc693f3
Homura-san <erickbispo@ufba.br>
2025-10-07 20:44:37 -0300

    Página não encontrada

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const NotFound = props => (
+    <div className="NotFound">
+        <h1>404</h1>
+        <h2>A página não foi encontrada.</h2>
+    </div>
+)
+
+export default NotFound;
